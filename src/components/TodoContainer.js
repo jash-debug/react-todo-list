1d457c62d4558b9ee7b95943037127f038558f90
jash-debug <jash-debug@github.com>
2022-12-21 08:23:27 +0300

    add linter configuration

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,30 +1,31 @@
-import React from "react"
-import TodosList from "./TodoList";
+import React from 'react';
+import TodosList from './TodoList';
 
 class TodoContainer extends React.Component {
     state = {
-        todos: [
-          {
-            id: 1,
-            title: "Setup development environment",
-            completed: true
-          },
-          {
-            id: 2,
-            title: "Develop website and add content",
-            completed: false
-          },
-          {
-            id: 3,
-            title: "Deploy to live server",
-            completed: false
-          }
-        ]
-       };
-  render() {
-    return (
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+
+    render() {
+      return (
         <TodosList todos={this.state.todos} />
-    );
-  }
+      );
+    }
 }
-export default TodoContainer
+export default TodoContainer;
